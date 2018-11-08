e983fd4a3cfae4868922801d81805c03db3844f6
Markus Oley <markus.oley@vsa.de>
2018-11-08 16:20:03 +0100

    demo data

--- spica-server/spica-server-application/src/main/java/org/spica/server/demodata/CustomerDemoData.java
+++ spica-server/spica-server-application/src/main/java/org/spica/server/demodata/CustomerDemoData.java
@@ -3,7 +3,10 @@ package org.spica.server.demodata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spica.server.commons.Role;
+import org.spica.server.project.domain.Project;
+import org.spica.server.project.domain.Topic;
 import org.spica.server.user.domain.User;
+import org.spica.server.user.domain.UserGroup;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,23 +14,45 @@ public class CustomerDemoData extends DemoDataCreator{
 
   private final static Logger LOGGER = LoggerFactory.getLogger(CustomerDemoData.class);
 
-  private final static String CUSTOMER_DEPARTMENT_1 = "Konzeption";
-  private final static String CUSTOMER_DEPARTMENT_2 = "Hotline";
+  private final static String ASSURANCE_DEPARTMENT = "Abteilung Versicherungen";
+  private final static String CAPITAL_DEPARTMENT = "Abteilung Vermoegen";
+
+  private final static String PROJECT_KUNDE1 = "Kunde Michael Mayer";
+  private final static String PROJECT_KUNDE2 = "Kunde Karin Klein";
+  private final static String PROJECT_KUNDE3 = "Kunde Caro Kloppenhardt";
+
+  private final static String ISSUE_NEW_CAR = "Neues Auto";
+  private final static String ISSUE_ANPASSUNG_VWL = "Anpassung VwL";
+  private final static String ISSUE_DISMISSAL = "Ich kuendige";
 
   public void create () {
     LOGGER.info("Start creating customer demo data");
 
-
-    User dev1 = user("Mayer", "Michael", Role.USER);
-    User dev2 = user("Klein", "Karin", Role.USER);
+    User customer1 = user("Mayer", "Michael", Role.USER);
+    User customer2 = user("Klein", "Karin", Role.USER);
+    User customer3 = user("Cloppenhardt", "Caro", Role.USER);
     User guest = user("Gast", "Gregor", Role.GUEST);
     User admin = user ("Admin", "Achim", Role.ADMIN);
-    User teamlead = user ("Lercher", "Lasse", Role.USER);
+    User service1 = user ("Lercher", "Lasse", Role.USER);
+    User service2 = user ("Service", "Simone", Role.USER);
 
-    userGroup(CUSTOMER_DEPARTMENT_1, dev1, false);
-    userGroup(CUSTOMER_DEPARTMENT_2, dev2, false);
+    UserGroup departmentAssurance = userGroup(ASSURANCE_DEPARTMENT, service1, true);
+    UserGroup departmentCapital = userGroup(CAPITAL_DEPARTMENT, service2, true);
 
-    userGroup(CUSTOMER_DEPARTMENT_1, teamlead, true);
-    userGroup(CUSTOMER_DEPARTMENT_2, admin, true);
+    Project projectCustomer1 = project(PROJECT_KUNDE1, admin, null, departmentAssurance);
+    Project projectCustomer2 = project(PROJECT_KUNDE2, admin, null, departmentAssurance, departmentCapital);
+    Project projectCustomer3 = project(PROJECT_KUNDE3, admin, null, departmentAssurance, departmentCapital);
+
+    Topic topicDismissal = topic(ISSUE_DISMISSAL, customer1, projectCustomer3);
+    Topic topicNewCar = topic(ISSUE_NEW_CAR, customer1, projectCustomer1);
+    Topic topicAnpassungVwl = topic(ISSUE_ANPASSUNG_VWL, service1, projectCustomer2);
+
+    meep(customer1, projectCustomer1, "Hallo, ich bin der Michael, und ich möchte Geld");
+    meep(customer1, topicNewCar, "Ich habe mir einen neuen Golf gekauft, könnt ihr mir bitte ein KFZ-Versicherungs Angebot machen?");
+
+    meep(customer2, topicDismissal, "Ich hab was besseres gefunden, ich kuendige");
+
+    meep(service1, topicAnpassungVwl, "Es gibt neue gesetzliche Bestimmungen zu Deinen Vwls, bitte mal bei mir vorbeischauen");
+    meep(customer2, topicAnpassungVwl, "Geht klar, bin morgen sowieso in der Gegend");
   }
 }
--- spica-server/spica-server-application/src/main/java/org/spica/server/demodata/DevelopmentDemoData.java
+++ spica-server/spica-server-application/src/main/java/org/spica/server/demodata/DevelopmentDemoData.java
@@ -3,7 +3,10 @@ package org.spica.server.demodata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spica.server.commons.Role;
+import org.spica.server.project.domain.Project;
+import org.spica.server.project.domain.Topic;
 import org.spica.server.user.domain.User;
+import org.spica.server.user.domain.UserGroup;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,23 +14,56 @@ public class DevelopmentDemoData extends DemoDataCreator{
 
   private final static Logger LOGGER = LoggerFactory.getLogger(DevelopmentDemoData.class);
 
-  private final static String DEVELOPEMENT_DEPARTMENT_1 = "Entwickler Berlin";
-  private final static String DEVELOPMEN_DEPARTMENT_2 = "Entwickler Frankfurt";
+  private final static String CONCEPTION_DEPARTMENT = "Konzeption";
+  private final static String HOTLINE_DEPARTMENT = "Hotline";
+  private final static String DEV_DEPARTMENT = "Development";
+
+  private final static String PRODUKT_1 = "Produkt 1";
+  private final static String PRODUKT_2 = "Produkt 2";
+
+  private final static String ISSUE_PERFORMANCE = "Performance ist zu schlecht";
+  private final static String ISSUE_USABILITY = "Usability ist wie Windows 95";
+  private final static String ISSUE_STABILITY = "Stabilitaet ist schlecht";
+
 
   public void create () {
     LOGGER.info("Start creating development demo data");
 
     User dev1 = user("Mayer", "Michael", Role.USER);
     User dev2 = user("Klein", "Karin", Role.USER);
+    User hotliner = user("Hotliner", "Holger", Role.USER);
+    User conception = user("Konzept", "Karin", Role.USER);
     User guest = user("Gast", "Gregor", Role.GUEST);
     User admin = user ("Admin", "Achim", Role.ADMIN);
     User teamlead = user ("Lercher", "Lasse", Role.USER);
 
-    userGroup(DEVELOPEMENT_DEPARTMENT_1, dev1, false);
-    userGroup(DEVELOPMEN_DEPARTMENT_2, dev2, false);
+    UserGroup departmentConception = userGroup(CONCEPTION_DEPARTMENT, conception, false);
+    UserGroup departmentHotline = userGroup(HOTLINE_DEPARTMENT, hotliner, false);
+    UserGroup departmentDevelopment = userGroup(DEV_DEPARTMENT, dev1, false);
+    userGroup(DEV_DEPARTMENT, dev2, false);
 
-    userGroup(DEVELOPEMENT_DEPARTMENT_1, teamlead, true);
-    userGroup(DEVELOPMEN_DEPARTMENT_2, admin, true);
+    userGroup(CONCEPTION_DEPARTMENT, teamlead, true);
+    userGroup(HOTLINE_DEPARTMENT, admin, true);
+
+    Project product1 = project(PRODUKT_1, admin, null, departmentConception, departmentDevelopment);
+    Project product2 = project(PRODUKT_2, admin, null, departmentConception, departmentHotline, departmentDevelopment);
+
+    Topic topicIssueStability = topic(ISSUE_STABILITY, guest, product1);
+    Topic topicIssuePerformance = topic(ISSUE_PERFORMANCE, guest, product2);
+    Topic topicIssueUsability = topic(ISSUE_USABILITY, teamlead, product2);
+
+    meep(guest, product1, "Hallo, ich habe Euer Produkt im Internet gesehen, kann ich das haben?");
+
+    meep(guest, topicIssuePerformance, "Warum ist mein Programm so langsam");
+    meep(hotliner, topicIssuePerformance, "Welche Version haben Sie?");
+    meep(guest, topicIssuePerformance, "0.8.15");
+    meep(hotliner, topicIssuePerformance, "@Mayer Kannst Du mal bitte schauen?");
+    meep(dev1, topicIssuePerformance, "Also auf meinem Rechner ist es rasendschnell, stellt ihm mal neue Hardware hin");
+
+    meep(guest, topicIssueStability, "Mein Tisch, auf dem mein Rechner mit dem Programm lief, ist zusammengebrochen. Was tun?");
+
+    meep(conception, topicIssueUsability, "Ich glaube die neue Version von unserem Produkt ist schlecht zu bedienen");
+
 
 
 
